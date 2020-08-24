7bf129a748abfc5692646632d3fa9d5220ae51de
Ryukyo <florian.fejer@hotmail.de>
2020-08-24 12:24:52 +0200

    Add actual files

---
+++ app/src/main/java/com/example/teamd/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.teamd
+
+import android.graphics.Typeface
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController!!.hide(WindowInsets.Type.statusBars())
+        } else {
+            // deprecated from API 30 onwards
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "njnaruto.ttf")
+        tv_app_name.typeface = typeFace
+    }
+}
