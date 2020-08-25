5561b73b36a6416a21b3a552a00d785a2b157292
Ryukyo <florian.fejer@hotmail.de>
2020-08-25 08:05:52 +0200

    Add sign up and sign in functionality

--- app/src/main/java/com/example/teamd/activities/SignInActivity.kt
+++ app/src/main/java/com/example/teamd/activities/SignInActivity.kt
@@ -1,17 +1,27 @@
 package com.example.teamd.activities
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.teamd.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        auth = FirebaseAuth.getInstance()
 
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -23,6 +33,11 @@ class SignInActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+
+        btn_sign_in.setOnClickListener{
+            signInRegisteredUser()
+        }
+
         setupActionBar()
     }
     private fun setupActionBar() {
@@ -36,5 +51,51 @@ class SignInActivity : AppCompatActivity() {
         }
 
         toolbar_sign_in_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun signInRegisteredUser() {
+        val email: String = et_email_sign_in.text.toString().trim { it <= ' ' }
+        val password: String = et_password_sign_in.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)) {
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+
+                        Log.d("Sign in", "signInWithEmail:success")
+                        val user = auth.currentUser
+
+                        Toast.makeText(
+                            this@SignInActivity,
+                            "You have successfully signed in.",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+                    } else {
+                        Log.w("Sign in", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    private fun validateForm(email: String, password: String): Boolean {
+        return if (TextUtils.isEmpty(email)) {
+            showErrorSnackBar("Please enter email.")
+            false
+        } else if (TextUtils.isEmpty(password)) {
+            showErrorSnackBar("Please enter password.")
+            false
+        } else {
+            true
+        }
     }
 }
--- app/src/main/java/com/example/teamd/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/teamd/activities/SignUpActivity.kt
@@ -3,12 +3,18 @@ package com.example.teamd.activities
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.teamd.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -37,5 +43,67 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         toolbar_sign_up_activity.setNavigationOnClickListener { onBackPressed() }
+
+        btn_sign_up.setOnClickListener { registerUser() }
+    }
+
+    private fun registerUser(){
+        val name: String = et_name.text.toString().trim { it <= ' ' }
+        val email: String = et_email.text.toString().trim { it <= ' ' }
+        val password: String = et_password.text.toString().trim { it <= ' ' }
+
+        if (validateForm(name, email, password)) {
+
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+                OnCompleteListener<AuthResult> { task ->
+
+                    hideProgressDialog()
+
+                    if (task.isSuccessful) {
+
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+                        val registeredEmail = firebaseUser.email!!
+
+                        Toast.makeText(
+                            this@SignUpActivity,
+                            "$name you have successfully registered with email id $registeredEmail.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        FirebaseAuth.getInstance().signOut()
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this@SignUpActivity,
+                            "Registration failed",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                })
+
+        }
+    }
+    /**
+     * A function to validate the entries of a new user.
+     */
+    private fun validateForm(name: String, email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter your name.")
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter your email.")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter your password.")
+                false
+            }
+            else -> {
+                true
+            }
+        }
     }
 }
