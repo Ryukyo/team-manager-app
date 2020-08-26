58ce60f3d3023d5116db121d3ca6ae313d581206
Ryukyo <florian.fejer@hotmail.de>
2020-08-26 16:10:44 +0200

    Add members to board and share access

--- app/src/main/java/com/example/teamd/activities/MembersActivity.kt
+++ app/src/main/java/com/example/teamd/activities/MembersActivity.kt
@@ -1,7 +1,11 @@
 package com.example.teamd.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.teamd.R
 import com.example.teamd.adapters.MemberListItemsAdapter
@@ -10,9 +14,11 @@ import com.example.teamd.models.Board
 import com.example.teamd.models.User
 import com.example.teamd.utils.Constants
 import kotlinx.android.synthetic.main.activity_members.*
+import kotlinx.android.synthetic.main.dialog_search_member.*
 
 class MembersActivity : BaseActivity() {
     private lateinit var mBoardDetails: Board
+    private lateinit var mAssignedMembersList:ArrayList<User>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +31,7 @@ class MembersActivity : BaseActivity() {
                 this@MembersActivity,
                 mBoardDetails.assignedTo
             )
-        } 
+        }
         setupActionBar()
     }
 
@@ -41,12 +47,66 @@ class MembersActivity : BaseActivity() {
         toolbar_members_activity.setNavigationOnClickListener { onBackPressed() }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add_member, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar menu items
+        when (item.itemId) {
+            R.id.action_add_member -> {
+                dialogSearchMember()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun dialogSearchMember() {
+        val dialog = Dialog(this)
+
+        dialog.setContentView(R.layout.dialog_search_member)
+        dialog.tv_add.setOnClickListener(View.OnClickListener {
+            val email = dialog.et_email_search_member.text.toString()
+
+            if (email.isNotEmpty()) {
+                dialog.dismiss()
+                showProgressDialog(resources.getString(R.string.please_wait))
+                FirestoreClass().getMemberDetails(this@MembersActivity, email)
+            } else {
+                showErrorSnackBar("Please enter members email address.")
+                /*Toast.makeText(
+                    this@MembersActivity,
+                    "Please enter members email address.",
+                    Toast.LENGTH_SHORT
+                ).show()*/
+            }
+        })
+        dialog.tv_cancel.setOnClickListener(View.OnClickListener {
+            dialog.dismiss()
+        })
+        dialog.show()
+    }
+
+    fun memberDetails(user: User) {
+        mBoardDetails.assignedTo.add(user.id)
+        FirestoreClass().assignMemberToBoard(this@MembersActivity, mBoardDetails, user)
+    }
+
     fun setupMembersList(list: ArrayList<User>) {
+        mAssignedMembersList = list
         hideProgressDialog()
         rv_members_list.layoutManager = LinearLayoutManager(this@MembersActivity)
         rv_members_list.setHasFixedSize(true)
 
         val adapter = MemberListItemsAdapter(this@MembersActivity, list)
         rv_members_list.adapter = adapter
+    }
+
+    fun memberAssignSuccess(user: User) {
+        hideProgressDialog()
+        mAssignedMembersList.add(user)
+        setupMembersList(mAssignedMembersList)
     }
 }
